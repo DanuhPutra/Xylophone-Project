92edde64b9ac214b9312febe9301becd1d6b3f7c
Ngurah Danuh Putra <ngurahdanuhputra@gmail.com>
2025-06-15 23:28:44 +0800

    fix another sound key

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,13 +18,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        playSound(soundName: sender.currentTitle!)
+        delayColor(for: sender)
     }
     
-    func playSound(){
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(soundName: String){
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
+        
+    }
+    
+    func delayColor(for button: UIButton){
+        UIView.animate(withDuration: 0.05) {
+            button.alpha = 0.5
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            UIView.animate(withDuration: 0.1) {
+                button.alpha = 1.0
+            }
+        }
     }
     
     
